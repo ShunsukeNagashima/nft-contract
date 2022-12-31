bcd39907bda86f03651ebc1f4739d9a9ec2a8c11
Shunsuke Nagashima <shun.n_dr@icloud.com>
2022-12-31 14:13:22 +0900

    chore: add some necessary packages and run compile

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
+require('hardhat-abi-exporter');
 
 const projectId = '2e0801dfeead43e69039a5fc4f2b3119';
 const privateKey = process.env.PRIVATE_KEY;
@@ -13,6 +14,10 @@ module.exports = {
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/0c324fc30b6348168989373a20717189",
       accounts: [privateKey],
     },
   },
